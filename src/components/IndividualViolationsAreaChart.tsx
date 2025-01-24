ca22cf28a82bac0454df84d2368af48694b0d9ce
timmatsam <timmatsam@gmail.com>
2025-01-24 18:51:17 -0500

    feat: add longitudinal area chart to ind violations page

---
+++ src/components/IndividualViolationsAreaChart.tsx
@@ -0,0 +1,161 @@
+"use client";
+
+import * as React from "react";
+import { Area, AreaChart, CartesianGrid, XAxis } from "recharts";
+
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import {
+  type ChartConfig,
+  ChartContainer,
+  ChartTooltip,
+  ChartTooltipContent,
+} from "@/components/ui/chart";
+
+const chartConfig = {
+  visitors: {
+    label: "Visitors",
+  },
+  totalViolations: {
+    label: "Total Violations",
+    color: "hsl(var(--chart-1))",
+  },
+} satisfies ChartConfig;
+
+interface IndividualViolationsAreaChartProps {
+  data: {
+    yearMonth: string;
+    totalViolations: number;
+  }[];
+}
+
+export function IndividualViolationsAreaChart({
+  data,
+}: IndividualViolationsAreaChartProps) {
+  // const [timeRange, setTimeRange] = React.useState("90d");
+
+  // const filteredData = data.filter((item) => {
+  //   const date = new Date(item.yearMonth);
+  //   const referenceDate = new Date("2024-06-30");
+  //   let daysToSubtract = 90;
+  //   if (timeRange === "30d") {
+  //     daysToSubtract = 30;
+  //   } else if (timeRange === "7d") {
+  //     daysToSubtract = 7;
+  //   }
+  //   const startDate = new Date(referenceDate);
+  //   startDate.setDate(startDate.getDate() - daysToSubtract);
+  //   return date >= startDate;
+  // });
+
+  return (
+    <Card className="mt-5">
+      <CardHeader className="flex items-center gap-2 space-y-0 border-b py-5 sm:flex-row">
+        <div className="grid flex-1 gap-1 text-center sm:text-left">
+          <CardTitle>Number of Violations Committed By Month</CardTitle>
+          {/* <CardDescription>
+            Trend of violations committed on a per month basis
+          </CardDescription> */}
+        </div>
+        {/* <Select value={timeRange} onValueChange={setTimeRange}>
+          <SelectTrigger
+            className="w-[160px] rounded-lg sm:ml-auto"
+            aria-label="Select a value"
+          >
+            <SelectValue placeholder="Last 3 months" />
+          </SelectTrigger>
+          <SelectContent className="rounded-xl">
+            <SelectItem value="90d" className="rounded-lg">
+              Last 3 months
+            </SelectItem>
+            <SelectItem value="30d" className="rounded-lg">
+              Last 30 days
+            </SelectItem>
+            <SelectItem value="7d" className="rounded-lg">
+              Last 7 days
+            </SelectItem>
+          </SelectContent>
+        </Select> */}
+      </CardHeader>
+      <CardContent className="px-2 pt-4 sm:px-6 sm:pt-6">
+        <ChartContainer
+          config={chartConfig}
+          className="aspect-auto h-[250px] w-full"
+        >
+          <AreaChart data={data}>
+            <defs>
+              <linearGradient
+                id="fillTotalViolations"
+                x1="0"
+                y1="0"
+                x2="0"
+                y2="1"
+              >
+                <stop
+                  offset="5%"
+                  stopColor="var(--color-totalViolations)"
+                  stopOpacity={0.8}
+                />
+                <stop
+                  offset="95%"
+                  stopColor="var(--color-totalViolations)"
+                  stopOpacity={0.1}
+                />
+              </linearGradient>
+              {/* <linearGradient id="fillMobile" x1="0" y1="0" x2="0" y2="1">
+                <stop
+                  offset="5%"
+                  stopColor="var(--color-mobile)"
+                  stopOpacity={0.8}
+                />
+                <stop
+                  offset="95%"
+                  stopColor="var(--color-mobile)"
+                  stopOpacity={0.1}
+                />
+              </linearGradient> */}
+            </defs>
+            <CartesianGrid vertical={false} />
+            <XAxis
+              dataKey="yearMonth"
+              tickLine={false}
+              axisLine={false}
+              tickMargin={8}
+              minTickGap={32}
+              tickFormatter={(value: string) => {
+                const date = new Date(value);
+                return date.toLocaleDateString("en-US", {
+                  year: "numeric",
+                  month: "short",
+                });
+              }}
+            />
+            <ChartTooltip
+              cursor={false}
+              content={
+                <ChartTooltipContent
+                  labelFormatter={(value: string) => {
+                    const date = new Date(value);
+                    return date.toLocaleDateString("en-US", {
+                      year: "numeric",
+                      month: "short",
+                    });
+                  }}
+                  hideIndicator
+                />
+              }
+            />
+            <Area
+              dataKey="totalViolations"
+              type="natural"
+              fill="url(#fillTotalViolations)"
+              stroke="var(--color-totalViolations)"
+              stackId="a"
+            />
+
+            {/* <ChartLegend content={<ChartLegendContent />} /> */}
+          </AreaChart>
+        </ChartContainer>
+      </CardContent>
+    </Card>
+  );
+}
