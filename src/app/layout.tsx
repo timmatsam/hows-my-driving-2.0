2a62f563cbee207f3ef6ad9e75dbcf39ff87ea09
timmatsam <timmatsam@gmail.com>
2025-02-09 18:11:19 -0500

    add viewport colors

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,12 +2,20 @@ import { cn } from "@/lib/utils";
 import "@/styles/globals.css";
 
 import { GeistSans } from "geist/font/sans";
-import { type Metadata } from "next";
+import { type Viewport, type Metadata } from "next";
 import { ThemeProvider } from "@/components/ThemeProvider";
+
 export const metadata: Metadata = {
   title: "How's My Driving 2",
   description: "View Parking and Camera Violations in NYC by Plate",
   icons: [{ rel: "icon", url: "/favicon.ico" }],
+};
+
+export const viewport: Viewport = {
+  themeColor: [
+    { media: "(prefers-color-scheme: light)", color: "white" },
+    { media: "(prefers-color-scheme: dark)", color: "#0A0A0A" },
+  ],
 };
 
 export default function RootLayout({
