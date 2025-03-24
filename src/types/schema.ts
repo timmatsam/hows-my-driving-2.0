77d4a2ac840bc8eb9ccfb0a3952b23745f99566f
timmatsam <timmatsam@gmail.com>
2025-03-24 09:42:35 -0400

    update validation error message

--- src/types/schema.ts
+++ src/types/schema.ts
@@ -1,6 +1,16 @@
 import { z } from "zod";
 
 export const featureRequestFormSchema = z.object({
-  title: z.string().min(2),
-  description: z.string().min(10),
+  title: z
+    .string()
+    .min(2, {
+      message: "Title is not long enough. ",
+    })
+    .describe("The title of the feature request"),
+  description: z
+    .string()
+    .min(10, {
+      message: "Please write more for the description.",
+    })
+    .describe("The description of the feature request"),
 });
